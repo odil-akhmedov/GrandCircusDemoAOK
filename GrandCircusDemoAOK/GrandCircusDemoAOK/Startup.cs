5f9e1fc6309ee06f92d1766a13432677e79b14cb
Odiljon Akhmedov <akhmedoff.odil@gmail.com>
2014-06-25 14:55:04 -0400

    Changed some content
    Added/changed content on the main page

---
+++ GrandCircusDemoAOK/GrandCircusDemoAOK/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(GrandCircusDemoAOK.Startup))]
+namespace GrandCircusDemoAOK
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
